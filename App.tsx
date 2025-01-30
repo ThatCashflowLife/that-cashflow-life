d664687bc6317a5465279f73e172c397abdaf54c
root <root@Vititoe-PC.>
2025-01-30 18:56:08 -0500

    fixed typescript err

--- App.tsx
+++ App.tsx
@@ -1,7 +1,32 @@
 // IGNORE THIS FILE ITS ONLY USED TO VIEW THE APP THROUGH SNACK EXPO
-import { ExpoRoot } from "expo-router";
+import { ExpoRoot } from 'expo-router';
+import { Platform } from 'react-native';
+
+// type declaration for require.context
+declare namespace NodeJS {
+  interface Require {
+    context: (
+      directory: string,
+      useSubdirectories?: boolean,
+      regExp?: RegExp
+    ) => {
+      keys(): string[];
+      <T>(id: string): T;
+      resolve(id: string): string;
+      id: string;
+    };
+  }
+}
 
 export default function App() {
-  const ctx = require.context("./app");
+  const getContext = Platform.select({
+    ios: () => require.context('./app'),
+    android: () => require.context('./app'),
+    web: () => require.context('./app'),
+  });
+
+  if (!getContext) return null;
+  const ctx = getContext();
+
   return <ExpoRoot context={ctx} />;
 }
