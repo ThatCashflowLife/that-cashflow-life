3820c65cfc3777c1144d4597bdee2bc11a589029
root <root@Vititoe-PC.>
2025-02-10 12:39:04 -0500

    more aliased paths

--- Professions/Lawyer.ts
+++ Professions/Lawyer.ts
@@ -1,4 +1,4 @@
-import profession from "@/interfaces/profession";
+import profession from "../interfaces/profession";
 
 const Lawyer: profession = {
   name: "Lawyer",
--- Professions/Police_Officer.ts
+++ Professions/Police_Officer.ts
@@ -1,4 +1,4 @@
-import profession from "@/interfaces/profession";
+import profession from "../interfaces/profession";
 
 const Police_Officer: profession = {
   name: "Police Officer",
