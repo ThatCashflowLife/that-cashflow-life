ee5a88bfd4a80c009a674bf759d4d9003ce8d451
Nathanvititoe <nathanvititoe@gmail.com>
2025-02-10 15:40:58 -0500

    added polyfixes

--- metro.config.ts
+++ metro.config.ts
@@ -1,6 +1,12 @@
-// FOR BUNDLING AND EXPO SNACK
-import { getDefaultConfig } from "expo/metro-config";
+const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
+
+// Add polyfills for Node.js modules
+config.resolver.extraNodeModules = {
+  path: require.resolve("path-browserify"),
+  crypto: require.resolve("crypto-browserify"),
+  stream: require.resolve("stream-browserify"),
+};
 
 module.exports = config;
